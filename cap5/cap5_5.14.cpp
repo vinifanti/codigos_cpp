08107e901c6c10882d0bda549cc6745e06578d79
Vinicius Fanti <vinicamposfanti@gmail.com>
2022-01-15 22:44:17 -0300

    cods

---
+++ cap5/cap5_5.14.cpp
@@ -0,0 +1,6 @@
+/*
+
+5.14-) Dados os lados 'a' e 'b' de um retangulo e o raio 'r' de um circulo, calcular e exibir a area e o perimetro das duas, indicando qual a figura de menor 
+       area e qual a figura de maior perimetro.
+
+*/
